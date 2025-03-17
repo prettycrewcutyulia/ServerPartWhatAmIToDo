4b8c1115f3447a03b37c944c5e9e12a04e036072
Гудошникова Юлия Владимировна <gudoshnikovajulia@yandex.ru>
2025-03-17 07:24:26 +0300

    Обновила чтение дат

--- ServerPartWhatAmIToDo/Models/Goal.cs
+++ ServerPartWhatAmIToDo/Models/Goal.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using ServerPartWhatAmIToDo.Models.Goals;
 
 namespace ServerPartWhatAmIToDo.Models;
@@ -13,7 +14,10 @@ public class Goal
     
     public List<Step> Steps { get; set; }
     
+    [JsonConverter(typeof(UnixDateTimeConverter))]
     public DateTime? StartDate { get; set; }
+    
+    [JsonConverter(typeof(UnixDateTimeConverter))]
     public DateTime? Deadline { get; set; }
     
 
@@ -48,4 +52,36 @@ public class Step
     public string GoalId { get; set; }
     public string Title { get; set; }
     public bool IsCompleted { get; set; }
+}
+
+public class UnixDateTimeConverter : JsonConverter<DateTime?>
+{
+    public override DateTime? ReadJson(JsonReader reader, Type objectType, DateTime? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null)
+        {
+            return null;
+        }
+
+        if (reader.TokenType == JsonToken.Integer)
+        {
+            long unixTime = (long)reader.Value;
+            return DateTimeOffset.FromUnixTimeSeconds(unixTime).DateTime;
+        }
+
+        throw new JsonSerializationException("Unexpected token type when parsing a DateTime.");
+    }
+
+    public override void WriteJson(JsonWriter writer, DateTime? value, JsonSerializer serializer)
+    {
+        if (value.HasValue)
+        {
+            long unixTime = ((DateTimeOffset)value.Value).ToUnixTimeSeconds();
+            writer.WriteValue(unixTime);
+        }
+        else
+        {
+            writer.WriteNull();
+        }
+    }
 }
